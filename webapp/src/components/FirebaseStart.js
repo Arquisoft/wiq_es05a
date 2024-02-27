f7ebb590cc0269585842fbb473589ef9b8dab8db
Fernando Jose Gonzalez Sierra <uo277938@uniovi.es>
2024-02-27 14:06:31 +0100

    Login y registro con Firebase
    Firebase Key no se sube al repositorio, actualmente en .env local

---
+++ webapp/src/components/FirebaseStart.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebase_key = process.env.REACT_APP_FIREBASE_KEY
+
+console.log(firebase_key)
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+apiKey: firebase_key,
+authDomain: "wiq5a-3f58f.firebaseapp.com",
+projectId: "wiq5a-3f58f",
+storageBucket: "wiq5a-3f58f.appspot.com",
+messagingSenderId: "604775470186",
+appId: "1:604775470186:web:f1b9f7e134e2ced1546613"
+};
+
+// Initialize Firebase
+const FirebaseStart = initializeApp(firebaseConfig);
+
+export default FirebaseStart;
