944166232f6e3f62650937481927f146605421ed
Lara <uo276026@uniovi.es>
2024-02-28 22:07:54 +0100

    Cambios en Front-end + docu

--- webapp/src/components/Pages/Juego.js
+++ webapp/src/components/Pages/Juego.js
@@ -1,20 +1,27 @@
 // src/components/Login.js
 import React, { useState } from 'react';
 import axios from 'axios';
+import '../Estilos/juego.css';
 import { Container, Typography, TextField, Button, Snackbar } from '@mui/material';
 
+const botonRespuesta = (event) => { 
+  //comprobara si la respuesta es correcta o no 
+  }
+
 const Juego = ({isLogged}) => {
-
-
   return (
     <Container component="main" maxWidth="xs" sx={{ marginTop: 4 }}>
       <h2> PREGUNTA </h2>
-      <div> RESP 1</div>
-      <div> RESP 2</div>
-      <div> RESP 3</div>
-      <div> RESP 4</div>
+      <div class="button-container">
+        <button id="boton1" class="button" onclick={botonRespuesta}>RESPUESTA PRUEBA PARA VER CUANTO OCUPA 1</button>
+        <button id="boton2" class="button" onclick={botonRespuesta}>RESPUESTA PRUEBA PARA VER UNA RESPUESTA LARGA</button>
+        <button id="boton3" class="button" onclick={botonRespuesta}>RESPUESTA PRUEBA JEJE</button>
+        <button id="boton4" class="button" onclick={botonRespuesta}>RESPUESTA PRUEBA PARA VER QUE TAL</button>
+      </div>
     </Container>
   );
 };
+
+
 
 export default Juego;
