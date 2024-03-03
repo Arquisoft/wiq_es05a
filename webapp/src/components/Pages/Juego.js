fbeab07f7cea586298b3794bb5f8fba627c4f5a4
Fernando Jose Gonzalez Sierra <uo277938@uniovi.es>
2024-03-03 15:04:34 +0100

    Conectar generador preguntas con front

--- webapp/src/components/Pages/Juego.js
+++ webapp/src/components/Pages/Juego.js
@@ -1,24 +1,68 @@
 // src/components/Login.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import '../Estilos/juego.css';
 import { Container, Typography, TextField, Button, Snackbar } from '@mui/material';
 
-const botonRespuesta = (event) => { 
-  //comprobara si la respuesta es correcta o no 
+const Juego = ({isLogged}) => {
+  const [pregunta, setPregunta] = useState("")
+  const [resCorr, setResCorr] = useState("")
+  const [resFalse, setResFalse] = useState([])
+  const [respondido, setRespodido] = useState(false)
+  const [victoria, setVictoria] = useState(false)
+
+  const botonRespuesta = (respuesta) => { 
+    //comprobara si la respuesta es correcta o no 
+    setRespodido(true)
+    if(respuesta == resCorr){
+      console.log("entro a respuesat correcta")
+      setVictoria(true)
+    }
+    else{
+      setVictoria(false)
+    }
+  };
+  
+  async function CargarPregunta(pregunta, resCorr, resFalse){
+    useEffect(() => {
+      fetch("http://localhost:2500/pregunta")
+        .then((res) => res.json())
+        .then((todo) => {
+          setPregunta(todo.question)
+          setResCorr(todo.answerGood)
+          setResFalse(todo.answers)
+        });
+    }, []);
+    console.log(pregunta);
+    console.log(resCorr);
+    console.log(resFalse)
   }
 
-const Juego = ({isLogged}) => {
+  CargarPregunta(pregunta, resCorr, resFalse);
+  
+  
   return (
-    <Container component="main" maxWidth="xs" sx={{ marginTop: 4 }}>
-      <h2> PREGUNTA </h2>
-      <div className="button-container">
-        <button id="boton1" className="button" onClick={botonRespuesta}>RESPUESTA PRUEBA PARA VER CUANTO OCUPA 1</button>
-        <button id="boton2" className="button" onClick={botonRespuesta}>RESPUESTA PRUEBA PARA VER UNA RESPUESTA LARGA</button>
-        <button id="boton3" className="button" onClick={botonRespuesta}>RESPUESTA PRUEBA JEJE</button>
-        <button id="boton4" className="button" onClick={botonRespuesta}>RESPUESTA PRUEBA PARA VER QUE TAL</button>
-      </div>
-    </Container>
+    <>
+    {respondido ? (
+      <>
+      {victoria ? (
+        <h2> ACERTASTE </h2>
+      ) : (
+        <h2> FALLASTE </h2>
+      )}
+      </>
+      ) : (
+      <Container component="main" maxWidth="xs" sx={{ marginTop: 4 }}>
+        <h2> {pregunta} </h2>
+        <div className="button-container">
+          <button id="boton1" className="button" onClick={() => botonRespuesta(resFalse[1])}>{resFalse[1]}</button>
+          <button id="boton2" className="button" onClick={() => botonRespuesta(resFalse[2])}>{resFalse[2]}</button>
+          <button id="boton3" className="button" onClick={() => botonRespuesta(resFalse[0])}>{resFalse[0]}</button>
+          <button id="boton4" className="button" onClick={() => botonRespuesta(resFalse[3])}>{resFalse[3]}</button>
+        </div>
+      </Container>
+      )}
+      </>
   );
 };
 
