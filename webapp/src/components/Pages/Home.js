5d2fddc3a292e155b68aa451eec4496dfcdd76ad
Lara <uo276026@uniovi.es>
2024-03-03 19:43:01 +0100

    Modificaciones front-end

--- webapp/src/components/Pages/Home.js
+++ webapp/src/components/Pages/Home.js
@@ -1,5 +1,28 @@
+// src/components/Login.js
+import React, { useState, useEffect } from 'react';
+import '../Estilos/juego.css';
+import { Container } from '@mui/material';
+
+function clickJugar() {
+  window.location.href = "game";
+}
+
+function clickEstadisticas() {
+  window.location.href = "stats";
+}
+
 const Home = () => {
-    return <h1>Home</h1>;
+  return (
+    <>
+      <Container component="main" maxWidth="xs" sx={{ marginTop: 4 }}>
+        <h1> WIQ_es05 </h1>
+        <div className="button-container">
+          <button id="botonJugar" className="button" onClick={() => clickJugar()}> JUGAR</button>
+          <button id="botonEstadisticas" className="button" onClick={() => clickEstadisticas()}> ESTADÍSTICAS</button>
+         </div>
+      </Container>
+      </>
+  );
   };
   
   export default Home;
